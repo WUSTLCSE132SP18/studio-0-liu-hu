5a754fb43995277b6dd3fbb98390aee35ce814ed
toby <toby@DESKTOP-1TGUCQ3.dhcp.wustl.edu>
2018-01-19 23:32:49 -0600

    d

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,14 @@
+
+const int doPin =13;
+void setup() {
+  pinMode(doPin,OUTPUT);
+   Serial.begin(9600);
+ }
+void loop() {
+  
+  digitalWrite(doPin,HIGH);
+  delay(100);
+  digitalWrite(doPin,LOW);
+  delay(1000); 
+ }
+
